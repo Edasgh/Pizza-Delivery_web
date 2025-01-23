b87dbeee14bb8341890ebdf3852a0e720e36f2b1
Edasgh <edas25564@gmail.com>
2025-01-23 17:20:41 +0530

    "updated"

--- Client/src/rootExports.js
+++ Client/src/rootExports.js
@@ -1,2 +1,2 @@
-export const BACKEND_BASE_URL = "https://pizza-delivery-web.onrender.com";
+export const BACKEND_BASE_URL = "https://pizza-delivery-web-backend.onrender.com";
 // export const BACKEND_BASE_URL = "http://localhost:8080";
