7f5206153912a9a21095c79c32934b34fd6d6bd4
Edasgh <edas25564@gmail.com>
2025-01-23 17:15:27 +0530

    "backend url deployed"

--- Client/src/rootExports.js
+++ Client/src/rootExports.js
@@ -1,1 +1,2 @@
-export const BACKEND_BASE_URL = "http://localhost:8080";
+export const BACKEND_BASE_URL = "https://pizza-delivery-web.onrender.com";
+// export const BACKEND_BASE_URL = "http://localhost:8080";
