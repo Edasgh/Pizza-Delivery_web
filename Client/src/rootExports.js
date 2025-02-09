b701842eecfc1cdf2a78fb1558eab67ee863ce72
Edasgh <edas25564@gmail.com>
2025-02-09 17:38:08 +0530

    "added backend url in env"

--- Client/src/rootExports.js
+++ Client/src/rootExports.js
@@ -1,2 +1,2 @@
-// export const BACKEND_BASE_URL = "https://pizza-delivery-web-backend.onrender.com";
-export const BACKEND_BASE_URL = "http://localhost:8080";
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
+// export const BACKEND_BASE_URL = "http://localhost:8080";
