ea4928e4ed62cd485262f4b963624e0f067e46b6
Edasgh <edas25564@gmail.com>
2025-02-12 19:04:04 +0530

    "valid redirect uri added"

--- Client/src/components/Auth/Login/Login.jsx
+++ Client/src/components/Auth/Login/Login.jsx
@@ -126,7 +126,7 @@ const Login = () => {
       }
     } else if (provider === "github") {
       const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
-      const redirectUri = "http://localhost:5173/login"; // Change to your actual redirect URI
+      const redirectUri = "https://pizza-delivery-web.onrender.com/login"; // Change to your actual redirect URI
       window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user:email`;
     }
   };
